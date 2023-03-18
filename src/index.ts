6ce9005bf8390e2eb2b98196bafbc7220017aa4d
Srinandan Komanduri <keida1412@gmail.com>
2023-03-18 16:49:54 -0400

    [Route]: Added Health check route

--- src/index.ts
+++ src/index.ts
@@ -17,6 +17,10 @@ mongoose
 
 app.use("/api", AuthRouter);
 
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
 app.listen(port, () => {
   console.log(`Server started at http://localhost:${port}`);
 });
