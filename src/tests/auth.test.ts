692af8d8a6c6b627cd9e2346be44813059619149
Srinandan Komanduri <keida1412@gmail.com>
2023-04-02 14:59:49 -0400

    [TEST]: Added tests for login API

--- src/tests/auth.test.ts
+++ src/tests/auth.test.ts
@@ -85,3 +85,58 @@ describe("POST /api/register", () => {
         }));
     });
 });
+
+describe("POST /api/login", () => {
+    it("should return 400 BAD REQUEST", async () => {
+        const res = await request(app)
+            .post("/api/login")
+            .send({
+                email: "",
+                password: ""
+            });
+        
+        expect(res.status).to.equal(400);
+        expect(JSON.stringify(res.body)).to.equal(JSON.stringify({
+            "error": true,
+            "message": "All fields are required",
+            "data": null
+        }));
+    });
+
+    it("should return 400 Invalid credentials", async () => {
+        const res = await request(app)
+            .post("/api/login")
+            .send({
+                email: "srinandan@gmail.com",
+                password: "pp"
+            });
+
+        expect(res.status).to.equal(400);
+        expect(JSON.stringify(res.body)).to.equal(JSON.stringify({
+            "error": true,
+            "message": "Invalid credentials",
+            "data": null
+        }));
+    });
+
+    it("should return 200 OK", async () => {
+        const res = await request(app)
+            .post("/api/login")
+            .send({
+                email: "srinandankomanduri@gmail.com",
+                password: "password"
+            });
+
+        expect(res.status).to.equal(200);
+        expect(JSON.stringify(res.body)).to.equal(JSON.stringify({
+            "error": false,
+            "message": "User logged in successfully",
+            "data": {
+                "email": "srinandankomanduri@gmail.com",
+                "token": generateToken({
+                    "email": "srinandankomanduri@gmail.com"
+                })
+            }
+        }));
+    });
+});
